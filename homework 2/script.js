a5d3b9a4c5eef6394cd16c3b638214144479d62f
ljupce111 <ljupcemadafaka@gmail.com>
2024-12-03 06:46:10 +0100

    Add files via upload

---
+++ homework 2/script.js
@@ -0,0 +1,38 @@
+let input = prompt("Enter a year:");
+console.log(input);
+if((input-4)%12==0){
+    console.log("Rat");
+}
+else if((input-4)%12==1){
+    console.log("Ox");
+}
+else if((input-4)%12==2){
+    console.log("Tiger");
+}
+else if((input-4)%12==3){
+    console.log("Rabbit");
+}
+else if((input-4)%12==4){
+    console.log("Dragon");
+}
+else if((input-4)%12==5){
+    console.log("Snake");
+}
+else if((input-4)%12==6){
+    console.log("Horse");
+}
+else if((input-4)%12==7){
+    console.log("Goat");
+}
+else if((input-4)%12==8){
+    console.log("Monkey");
+}
+else if((input-4)%12==9){
+    console.log("Rooster");
+}
+else if((input-4)%12==10){
+    console.log("Dog");
+}
+else if((input-4)%12==11){
+    console.log("Pig");
+}
