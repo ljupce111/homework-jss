ae92e1003644e18933899c5dcee2be98ed933a67
ljupce111 <ljupcemadafaka@gmail.com>
2024-12-10 07:02:51 +0100

    Update script.js

--- homework 3/homework04/script.js
+++ homework 3/homework04/script.js
@@ -1,5 +1,6 @@
 let currentYear = new Date().getFullYear();
 let BirthYear=prompt(`We are in the year ${currentYear} \nEnter the year you were born in`);
+let BirthYearParse=parseInt(BirthYear);
 if(BirthYear>currentYear){
     alert("You might have entered the wrong year \nIf you would like to try again please refresh the page");
 }
